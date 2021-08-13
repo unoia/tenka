063d4c23356bcb2b603e958c692436e6dfe62e84
Ongki Herlambang <meepomodeon@gmail.com>
2021-08-14 05:45:45 +0700

    housekeeping: update deps

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,80 +14,48 @@ const Home = () => {
       </Head>
 
       <main>
-        <Text heading1 as="h1" truncate={2}>
-          This is tenka heading 1 text This is tenka heading 1 text This is
-          tenka heading 1 text This is tenka heading 1 text This is tenka
-          heading 1 text This is tenka heading 1 text This is tenka heading 1
-          text
+        <Text heading1 as="h1">
+          Hello there, general tenka heading1 text
         </Text>
-        <Text heading2 as="h2" truncate={2}>
-          This is tenka heading 1 text This is tenka heading 1 text This is
-          tenka heading 1 text This is tenka heading 1 text This is tenka
-          heading 1 text This is tenka heading 1 text This is tenka heading 1
-          text
+        <Text heading2 as="h2">
+          Hello there, general tenka heading2 text
         </Text>
-        <Text heading3 as="h3" truncate={2}>
-          This is tenka heading 1 text This is tenka heading 1 text This is
-          tenka heading 1 text This is tenka heading 1 text This is tenka
-          heading 1 text This is tenka heading 1 text This is tenka heading 1
-          text
+        <Text heading3 as="h3">
+          Hello there, general tenka heading3 text
         </Text>
-        <Text heading4 as="h4" truncate={2}>
-          This is tenka heading 1 text This is tenka heading 1 text This is
-          tenka heading 1 text This is tenka heading 1 text This is tenka
-          heading 1 text This is tenka heading 1 text This is tenka heading 1
-          text
+        <Text heading4 as="h4">
+          Hello there, general tenka heading4 text
         </Text>
-        <Text heading5 as="h5" truncate={2}>
-          This is tenka heading 1 text This is tenka heading 1 text This is
-          tenka heading 1 text This is tenka heading 1 text This is tenka
-          heading 1 text This is tenka heading 1 text This is tenka heading 1
-          text
+        <Text heading5 as="h5">
+          Hello there, general tenka heading5 text
         </Text>
-        <Text heading6 as="h6" truncate={2}>
-          This is tenka heading 1 text This is tenka heading 1 text This is
-          tenka heading 1 text This is tenka heading 1 text This is tenka
-          heading 1 text This is tenka heading 1 text This is tenka heading 1
-          text
-        </Text>
-        <Text xlarge as="p" truncate={2}>
-          This is tenka heading 1 text This is tenka heading 1 text This is
-          tenka heading 1 text This is tenka heading 1 text This is tenka
-          heading 1 text This is tenka heading 1 text This is tenka heading 1
-          text
-        </Text>
-        <Text large as="p" truncate={2}>
-          This is tenka heading 1 text This is tenka heading 1 text This is
-          tenka heading 1 text This is tenka heading 1 text This is tenka
-          heading 1 text This is tenka heading 1 text This is tenka heading 1
-          text
-        </Text>
-        <Text as="p" truncate={2}>
-          This is tenka heading 1 text This is tenka heading 1 text This is
-          tenka heading 1 text This is tenka heading 1 text This is tenka
-          heading 1 text This is tenka heading 1 text This is tenka heading 1
-          text
-        </Text>
-        <Text small as="p" truncate={2}>
-          This is tenka heading 1 text This is tenka heading 1 text This is
-          tenka heading 1 text This is tenka heading 1 text This is tenka
-          heading 1 text This is tenka heading 1 text This is tenka heading 1
-          text
+        <Text heading6 as="h6">
+          Hello there, general tenka heading6 text
         </Text>
         <Text display1 accent as="p" truncate>
-          This is tenka's display1 text and it is so big that you will need some
-          clamping to multiple lines so you can shot it properly!
+          This is tenka&apos;s display1 text and it is so big that you will need
+          some clamping to multiple lines so you can shot it properly!
         </Text>
         <Text display2 as="p" truncate>
-          This is tenka heading 1 text This is tenka heading 1 text This is
-          tenka heading 1 text This is tenka heading 1 text This is tenka
-          heading 1 text This is tenka heading 1 text This is tenka heading 1
-          text
+          This is tenka&apos;s display2 text and it is so big that you will need
+          some clamping to multiple lines so you can shot it properly!
         </Text>
         <Text display3 info as="p" truncate={1}>
-          Thys is tenka jump display3 text and it is big and you can see how it
-          is clamped into a double button text and see how the bottom text is
-          going here!
+          This is tenka&apos;s display3 text and it is so big that you will need
+          some clamping to multiple lines so you can shot it properly!
+        </Text>
+        <Text xlarge as="p">
+          Use xlarge for a big body text, this one here is the example
+        </Text>
+        <Text large as="p">
+          And here we have the large body text. It&apos;s nice, isn&apos;t it?
+        </Text>
+        <Text as="p">
+          Here goes the medium body text and this one is the default
+        </Text>
+        <Text small as="p">
+          While this text is very small and kinda hard to read, it&apos;s still
+          here!
         </Text>
       </main>
     </div>
